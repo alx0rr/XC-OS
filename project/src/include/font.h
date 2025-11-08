9d1a1d61a653ae3dfea94f7683b381818c6351e0
shinjiteta <iebrufor@gmail.com>
2025-11-08 22:25:18 +0200

    Add files via upload

---
+++ project/src/include/font.h
@@ -0,0 +1,3 @@
+#pragma once
+#include <stdint.h>
+extern const uint32_t font_bitmap[128][16];
---
+++ project/src/include/vbe.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <stdint.h>
+
+#define VBE_MODE 0x4118
+
+typedef struct {
+    uint16_t mode_attributes;
+    uint8_t  window_a_attr;
+    uint8_t  window_b_attr;
+    uint16_t window_granularity;
+    uint16_t window_size;
+    uint16_t window_a_segment;
+    uint16_t window_b_segment;
+    uint32_t window_function;
+    uint16_t bytes_per_scanline;
+    uint16_t width;
+    uint16_t height;
+    uint8_t  char_width;
+    uint8_t  char_height;
+    uint8_t  planes;
+    uint8_t  bpp;
+    uint8_t  banks;
+    uint8_t  memory_model;
+    uint8_t  bank_size;
+    uint8_t  image_pages;
+    uint8_t  reserved1;
+    uint8_t  red_mask_size;
+    uint8_t  red_field_pos;
+    uint8_t  green_mask_size;
+    uint8_t  green_field_pos;
+    uint8_t  blue_mask_size;
+    uint8_t  blue_field_pos;
+    uint8_t  reserved_mask_size;
+    uint8_t  reserved_field_pos;
+    uint8_t  direct_color_info;
+    uint32_t framebuffer;
+    uint32_t off_screen_mem;
+    uint16_t off_screen_mem_size;
+    uint8_t  reserved[206];
+} __attribute__((packed)) vbe_mode_info_t;
+
+typedef struct {
+    uint16_t width;
+    uint16_t height;
+    uint16_t pitch;
+    uint8_t bpp;
+    uint32_t* framebuffer;
+} vbe_info_t;
+
+extern vbe_mode_info_t vbe_mode_info_data;
+
+void vbe_init();
+void vbe_fill(uint32_t color);
+void vbe_rect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint32_t color);
+void vbe_putchar_16(char c, uint16_t x, uint16_t y, uint32_t fg, uint32_t bg);
+void printx(const char* str);
+void vbe_set_color(uint32_t fg, uint32_t bg);
+void vbe_clear();
+uint16_t vbe_get_width();
+uint16_t vbe_get_height();
