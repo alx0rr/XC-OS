95ddee3aab0a556bd39aedbb19c78fb119c56e75
forker-25 <gerni780@gmail.com>
2025-11-11 10:19:47 +0200

    Refactor init_vbe_mode by removing comments
    Removed comments in Russian and cleaned up code.

--- project/src/boot/utils/vbe.asm
+++ project/src/boot/utils/vbe.asm
@@ -1,15 +1,10 @@
 VBE_INFO_ADDR equ 0x5000
 
-; Инициализация VBE режима
-; Вход: cx - режим VBE
-; Выход: ax - результат (0x004F если успешно)
-; Использует: es, di
 init_vbe_mode:
     push es
     push di
     push cx
-    
-    ; Получаем информацию о режиме
+
     xor ax, ax
     mov es, ax
     mov di, VBE_INFO_ADDR       
@@ -20,10 +15,9 @@ init_vbe_mode:
     cmp ax, 0x004F
     jne .vbe_error
     
-    ; Устанавливаем режим
     mov ax, 0x4F02
     mov bx, cx
-    or bx, 0x4000              ; Установить бит 14 (линейный framebuffer)
+    or bx, 0x4000
     int 0x10
     
     cmp ax, 0x004F
