a4e8479575cf41e2559e6d880328b83c17954dcb
forker-25 <gerni780@gmail.com>
2026-01-04 15:49:00 +0000

    Auto-commit: 2026-01-04 15:48:59Z

--- project/src/drivers/text/text.c
+++ project/src/drivers/text/text.c
@@ -5,39 +5,33 @@
 
 #define RGB(r, g, b) ((uint32_t)(((r) << 16) | ((g) << 8) | (b)))
 
-uint16_t xpos = 0;
-uint16_t ypos = 0;
-uint32_t bg_color = 0;
-uint32_t fg_color = 0xFFFFFF;
+static uint16_t xpos = 0;
+static uint16_t ypos = 0;
+static uint32_t bg_color = 0;
+static uint32_t fg_color = 0xFFFFFF;
 
 static const uint8_t MY_FONT_W = 8;
 static const uint8_t MY_FONT_H = 13;
-static const int MY_BOLD_PERCENT = 5;
+static const int BOLD_PERCENT = 2;
 static uint8_t char_width = MY_FONT_W;
 static uint8_t line_height = MY_FONT_H + 1;
 static int font_metrics_inited = 0;
 
 static void init_font_metrics() {
     if (font_metrics_inited) return;
-    int extra_h = (MY_FONT_W * MY_BOLD_PERCENT + 50) / 100;
-    int extra_v = (MY_FONT_H * MY_BOLD_PERCENT + 50) / 100;
+    int extra_h = (MY_FONT_W * BOLD_PERCENT + 99) / 100;
+    int extra_v = (MY_FONT_H * BOLD_PERCENT + 99) / 100;
+    if (extra_h < 0) extra_h = 0;
+    if (extra_v < 0) extra_v = 0;
     char_width = MY_FONT_W + extra_h;
     line_height = MY_FONT_H + 1 + extra_v;
     font_metrics_inited = 1;
 }
 
-void bitmapblt(
-    uint16_t x,
-    uint16_t y,
-    uint8_t h,
-    const uint8_t* bitpattern,
-    uint32_t fore_color,
-    uint32_t back_color
-    )
-{
+void bitmapblt(uint16_t x, uint16_t y, uint8_t h, const uint8_t* bitpattern, uint32_t fore_color, uint32_t back_color) {
     init_font_metrics();
-    int extra_h = (MY_FONT_W * MY_BOLD_PERCENT + 50) / 100;
-    int extra_v = (h * MY_BOLD_PERCENT + 50) / 100;
+    int extra_h = (MY_FONT_W * BOLD_PERCENT + 99) / 100;
+    int extra_v = (h * BOLD_PERCENT + 99) / 100;
     int total_w = MY_FONT_W + extra_h;
     int total_h = h + extra_v;
     uint32_t fbw = fb_get_width();
@@ -49,25 +43,23 @@ void bitmapblt(
     }
     for (uint32_t row = 0; row < h; row++) {
         uint8_t pattern = bitpattern[row];
-        int colx = 0;
-        for (int bit = 7; bit >= 0; bit--) {
-            int set = (pattern >> bit) & 1;
-            if (set) {
+        for (int col = 0; col < (int)MY_FONT_W; col++) {
+            int bit = (pattern >> (7 - col)) & 1;
+            if (bit) {
                 for (int dy = 0; dy <= extra_v; dy++) {
                     for (int dx = 0; dx <= extra_h; dx++) {
-                        int px = x + colx + dx;
+                        int px = x + col + dx;
                         int py = y + row + dy;
                         if (px >= 0 && px < (int)fbw && py >= 0 && py < (int)fbh) fb_putpixel(px, py, fore_color);
                     }
                 }
             }
-            colx++;
         }
     }
 }
 
 void drawchar_at_pos(char c, uint16_t x, uint16_t y, uint32_t fore_color, uint32_t back_color) {
-    bitmapblt(x, y, MY_FONT_H, &FontData[(uint32_t)(uint8_t)c * MY_FONT_H], fore_color, back_color);
+    bitmapblt(x, y, MY_FONT_H, &FontData[(size_t)(uint8_t)c * MY_FONT_H], fore_color, back_color);
 }
 
 void putchar(char c) {
@@ -184,8 +176,7 @@ void printf(const char *format, ...) {
     }
 }
 
-void clear(){
-    init_font_metrics();
+void clear() {
     fb_fill(RGB(0, 0, 0));
     xpos = 0;
     ypos = 0;
