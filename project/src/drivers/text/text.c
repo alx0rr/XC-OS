d4b2d0a1e3819fba2608bdd1309251bed9b1633f
forker-25 <gerni780@gmail.com>
2026-01-04 16:26:59 +0000

    Auto-commit: 2026-01-04 16:26:59Z

--- project/src/drivers/text/text.c
+++ project/src/drivers/text/text.c
@@ -68,34 +68,35 @@ void drawchar_at_pos(
 }
 
 void putchar(char c) {
-	if (c == '\n') {
-		xpos = 0;
-		ypos += CHAR_HEIGHT;
-		if (ypos + CHAR_HEIGHT >= fb_get_height() - Y_MARGIN) {
+    if (c == '\n') {
+        xpos = X_MARGIN;
+        ypos += CHAR_HEIGHT + 4;
+        if (ypos + CHAR_HEIGHT >= fb_get_height() - Y_MARGIN) {
             fb_fill(bg_color);
             xpos = X_MARGIN;
             ypos = Y_MARGIN;
         }
-	} else if (c == '\r') {
-		xpos = 0;
-	} else if (c == '\b') {
-		if (xpos >= CHAR_WIDTH) {
-			xpos -= CHAR_WIDTH;
-		}
-	} else {
-		drawchar_at_pos(c, xpos, ypos, fg_color, bg_color);
-		xpos += CHAR_WIDTH;
-		if (xpos >= fb_get_width()) {
-			xpos = 0;
-			ypos += CHAR_HEIGHT;
-			if (ypos + CHAR_HEIGHT >= fb_get_height()) {
-				fb_fill(bg_color);
-				xpos = 0;
-				ypos = 0;
-			}
-		}
-	}
+    } else if (c == '\r') {
+        xpos = X_MARGIN;
+    } else if (c == '\b') {
+        if (xpos > X_MARGIN) {
+            xpos -= CHAR_WIDTH;
+        }
+    } else {
+        drawchar_at_pos(c, xpos, ypos, fg_color, bg_color);
+        xpos += CHAR_WIDTH;
+        if (xpos + CHAR_WIDTH >= fb_get_width() - X_MARGIN) {
+            xpos = X_MARGIN;
+            ypos += CHAR_HEIGHT + 4;
+            if (ypos + CHAR_HEIGHT >= fb_get_height() - Y_MARGIN) {
+                fb_fill(bg_color);
+                xpos = X_MARGIN;
+                ypos = Y_MARGIN;
+            }
+        }
+    }
 }
+
 
 void print_at_pos(const char* str, uint16_t x, uint16_t y, uint32_t fore_color, uint32_t back_color) {
 	while (*str != '\0') {
