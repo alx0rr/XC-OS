a0808f6d9009a982aea3df548418c78dda554b30
forker-25 <gerni780@gmail.com>
2026-01-04 15:56:37 +0000

    Auto-commit: 2026-01-04 15:56:37Z

--- project/src/drivers/text/text.c
+++ project/src/drivers/text/text.c
@@ -14,6 +14,7 @@ uint16_t xpos = 0;
 uint16_t ypos = 0;
 uint32_t bg_color = 0;
 uint32_t fg_color = 0xFFFFFF;
+uint8_t font_weight = 2;
 
 void bitmapblt(
 	uint16_t x,
@@ -21,28 +22,28 @@ void bitmapblt(
 	uint8_t h,
 	uint8_t* bitpattern,
 	uint32_t fore_color,
-	uint32_t back_color
+	uint32_t back_color,
+	uint8_t weight
 )
 {
 	uint16_t xx;
-
 	uint16_t yy = y;
 	for (uint32_t j = 0; j < h; j++) {
-
 		xx = x;
-		for (uint32_t i = 128; i > 0; i >>= 1)
-		{
-
-			if (i & *bitpattern)
-				fb_putpixel(xx, yy, fore_color);
-			else
-				fb_putpixel(xx, yy, back_color);
-
-			xx++;
+		for (uint32_t i = 128; i > 0; i >>= 1) {
+			if (i & *bitpattern) {
+				for (uint8_t wy = 0; wy < weight; wy++)
+					for (uint8_t wx = 0; wx < weight; wx++)
+						fb_putpixel(xx + wx, yy + wy, fore_color);
+			} else {
+				for (uint8_t wy = 0; wy < weight; wy++)
+					for (uint8_t wx = 0; wx < weight; wx++)
+						fb_putpixel(xx + wx, yy + wy, back_color);
+			}
+			xx += weight;
 		}
-
 		bitpattern++;
-		yy++;
+		yy += weight;
 	}
 }
 
@@ -54,14 +55,14 @@ void drawchar_at_pos(
 	uint32_t back_color
 )
 {
-	bitmapblt(x, y, 13, &FontData[(uint32_t)c * 13], fore_color, back_color);
+	bitmapblt(x, y, 13, &FontData[(uint32_t)c * 13], fore_color, back_color, font_weight);
 }
 
 void putchar(char c) {
 	if (c == '\n') {
 		xpos = 0;
-		ypos += 14;
-		if (ypos + 14 >= fb_get_height()) {
+		ypos += 14 * font_weight;
+		if (ypos + 14 * font_weight >= fb_get_height()) {
 			fb_fill(bg_color);
 			xpos = 0;
 			ypos = 0;
@@ -69,17 +70,17 @@ void putchar(char c) {
 	} else if (c == '\r') {
 		xpos = 0;
 	} else if (c == '\b') {
-		if (xpos >= 8) {
-			xpos -= 8;
+		if (xpos >= 8 * font_weight) {
+			xpos -= 8 * font_weight;
 			drawchar_at_pos(' ', xpos, ypos, fg_color, bg_color);
 		}
 	} else {
 		drawchar_at_pos(c, xpos, ypos, fg_color, bg_color);
-		xpos += 8;
+		xpos += 8 * font_weight;
 		if (xpos >= fb_get_width()) {
 			xpos = 0;
-			ypos += 14;
-			if (ypos + 14 >= fb_get_height()) {
+			ypos += 14 * font_weight;
+			if (ypos + 14 * font_weight >= fb_get_height()) {
 				fb_fill(bg_color);
 				xpos = 0;
 				ypos = 0;
@@ -91,7 +92,7 @@ void putchar(char c) {
 void print_at_pos(const char* str, uint16_t x, uint16_t y, uint32_t fore_color, uint32_t back_color) {
 	while (*str != '\0') {
 		drawchar_at_pos(*str, x, y, fore_color, back_color);
-		x += 8;
+		x += 8 * font_weight;
 		str++;
 	}
 }
@@ -105,83 +106,65 @@ void printf(const char *format, ...) {
 
 	while ((c = *format++) != '\0') {
 		if (c == '{' && strncmp(format, "BG(", 3) == 0) {
-
 			format += 3;
 			int r = 0, g = 0, b = 0;
-
 			r = 0;
 			while (*format >= '0' && *format <= '9') {
 				r = r * 10 + (*format - '0');
 				format++;
 			}
-
 			if (*format == ',') format++;
-
 			g = 0;
 			while (*format >= '0' && *format <= '9') {
 				g = g * 10 + (*format - '0');
 				format++;
 			}
-
 			if (*format == ',') format++;
-
 			b = 0;
 			while (*format >= '0' && *format <= '9') {
 				b = b * 10 + (*format - '0');
 				format++;
 			}
-
 			if (*format == ')') format++;
 			if (*format == '}') format++;
-
 			bg_color = RGB(r, g, b);
 		} else if (c == '{' && strncmp(format, "FG(", 3) == 0) {
 			format += 3;
 			int r = 0, g = 0, b = 0;
-
 			r = 0;
 			while (*format >= '0' && *format <= '9') {
 				r = r * 10 + (*format - '0');
 				format++;
 			}
-
 			if (*format == ',') format++;
-
 			g = 0;
 			while (*format >= '0' && *format <= '9') {
 				g = g * 10 + (*format - '0');
 				format++;
 			}
-
 			if (*format == ',') format++;
-
 			b = 0;
 			while (*format >= '0' && *format <= '9') {
 				b = b * 10 + (*format - '0');
 				format++;
 			}
-
 			if (*format == ')') format++;
 			if (*format == '}') format++;
-
 			fg_color = RGB(r, g, b);
 		} else if (c != '%') {
 			putchar(c);
 		} else {
 			char *p, *p2;
 			int pad0 = 0, pad = 0;
-
 			c = *format++;
 			if (c == '0') {
 				pad0 = 1;
 				c = *format++;
 			}
-
 			if (c >= '0' && c <= '9') {
 				pad = c - '0';
 				c = *format++;
 			}
-
 			switch (c) {
 				case 'd':
 				case 'u':
@@ -204,7 +187,6 @@ void printf(const char *format, ...) {
 					if (!p) {
 						p = "(null)";
 					}
-
 				string:
 					for (p2 = p; *p2; p2++);
 					for (; p2 < p + pad; p2++)
@@ -212,7 +194,6 @@ void printf(const char *format, ...) {
 					while (*p)
 						putchar(*p++);
 					break;
-
 				default:
 					putchar(c);
 					break;
