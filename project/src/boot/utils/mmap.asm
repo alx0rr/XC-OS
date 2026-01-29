e9ae54c537a49c2000ba0ae645abf898f08ee05d
shinjiteta <iebrufor@gmail.com>
2026-01-29 09:28:44 +0000

    Auto-commit: 2026-01-29 09:28:43Z

--- project/src/boot/utils/mmap.asm
+++ project/src/boot/utils/mmap.asm
@@ -1,74 +1,57 @@
 [bits 16]
 
 MMAP_ADDR equ 0x8000
-MMAP_BUFFER equ 0x9000
-MAX_MMAP_ENTRIES equ 128
+COUNT_ADDR equ 0x7FFC
+MAX_ENTRIES equ 128
 
 get_memory_map:
     push bp
     mov bp, sp
     push bx
-    push cx
-    push dx
     push si
     push di
-    
+    push dx
+    push cx
+    push ax
+
     xor eax, eax
+    mov ax, 0
     mov es, ax
     mov ds, ax
     xor ebx, ebx
-    xor bp, bp
-    mov dword [0x7FFC], 0
-    
-.mmap_loop:
-    mov di, MMAP_BUFFER
-    
-    mov edx, 0x534D4150
+    mov di, MMAP_ADDR
+    mov word [COUNT_ADDR], 0
+    mov word [COUNT_ADDR+2], 0
+
+.e820_loop:
     mov eax, 0xE820
+    mov edx, 0x534D4150
     mov ecx, 24
     int 0x15
-    
-    jc .mmap_done
-    
+    jc .e820_done
     cmp eax, 0x534D4150
-    jne .mmap_done
+    jne .e820_done
 
-    cmp ecx, 20
-    jl .check_next
-    
-    mov si, MMAP_BUFFER
-    mov ax, bp
-    mov cx, 20
-    mul cx
-    mov di, MMAP_ADDR
-    add di, ax
-    
-    mov cx, 20
-.copy_loop:
-    mov al, [si]
-    mov [di], al
-    inc si
-    inc di
-    dec cx
-    jnz .copy_loop
-    
-    inc bp
-    
-.check_next:
-    cmp bp, MAX_MMAP_ENTRIES - 1
-    jge .mmap_done
-    
+    mov ax, [COUNT_ADDR]
+    inc ax
+    cmp ax, MAX_ENTRIES
+    jae .e820_done
+    mov [COUNT_ADDR], ax
+    add di, 24
+
     test ebx, ebx
-    jne .mmap_loop
+    jnz .e820_loop
 
-.mmap_done:
-    movzx eax, bp
-    mov dword [0x7FFC], eax
-    
+.e820_done:
+    mov ax, [COUNT_ADDR]
+    mov word [COUNT_ADDR], ax
+    mov word [COUNT_ADDR+2], 0
+
+    pop ax
+    pop cx
+    pop dx
     pop di
     pop si
-    pop dx
-    pop cx
     pop bx
     pop bp
     ret
