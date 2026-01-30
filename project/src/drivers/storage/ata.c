6c299e2dd316417adebd68f6621934512dfed8d1
shinjiteta <iebrufor@gmail.com>
2026-01-30 12:12:57 +0000

    Auto-commit: 2026-01-30 12:12:57Z

--- project/src/drivers/storage/ata.c
+++ project/src/drivers/storage/ata.c
@@ -48,8 +48,6 @@ static void ata_wait_irq(void) {
 }
 
 static int ata_detect(uint16_t io_base, uint16_t ctrl_base, uint8_t master) {
-    outb(ctrl_base, 0x02);
-    
     outb(io_base + ATA_REG_DRIVE, master ? ATA_MASTER : ATA_SLAVE);
     ata_delay(io_base);
     
@@ -58,12 +56,10 @@ static int ata_detect(uint16_t io_base, uint16_t ctrl_base, uint8_t master) {
     
     uint8_t status = inb(io_base + ATA_REG_STATUS);
     if(status == 0) {
-        outb(ctrl_base, 0x00);
         return 0;
     }
     
     if(ata_wait(io_base, ATA_SR_DRQ, ATA_SR_DRQ, 100000) < 0) {
-        outb(ctrl_base, 0x00);
         return 0;
     }
     
@@ -71,16 +67,12 @@ static int ata_detect(uint16_t io_base, uint16_t ctrl_base, uint8_t master) {
     for(int i = 0; i < 256; i++)
         data[i] = inw(io_base + ATA_REG_DATA);
     
-    outb(ctrl_base, 0x00);
     return 1;
 }
 
 void ata_init(void) {
     idt_register_irq_handler(14, ata_irq_handler_primary);
     idt_register_irq_handler(15, ata_irq_handler_secondary);
-    
-    outb(ATA_PRIMARY_CTRL, 0x02);
-    outb(ATA_SECONDARY_CTRL, 0x02);
     
     ata_devices[0].io_base = ATA_PRIMARY_IO;
     ata_devices[0].ctrl_base = ATA_PRIMARY_CTRL;
@@ -101,9 +93,6 @@ void ata_init(void) {
     ata_devices[3].ctrl_base = ATA_SECONDARY_CTRL;
     ata_devices[3].master = 0;
     ata_devices[3].exists = ata_detect(ATA_SECONDARY_IO, ATA_SECONDARY_CTRL, 0);
-    
-    outb(ATA_PRIMARY_CTRL, 0x00);
-    outb(ATA_SECONDARY_CTRL, 0x00);
 }
 
 int ata_read_sector(uint8_t drive, uint32_t lba, uint8_t* buffer) {
