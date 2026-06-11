a24f0f019bd83a6cea0e59fe2c35a78f993574d6
Forker 25 <219643110+forker-25@users.noreply.github.com>
2026-06-11 23:02:33 +0000

    auto: 2026-06-11 23:02:33Z

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -6,6 +6,6 @@ void kernel_main(void) {
     startup();
 
     printf("{FG(0,255,0)}Welcome to {FG(194,122,255)}XC-OS {FG(0,255,0)}\n");
-
+    printf("{FG(0,255,0)}Не обращай внимание, это тест кириллицы\n");
     kshell_launch();
 }
